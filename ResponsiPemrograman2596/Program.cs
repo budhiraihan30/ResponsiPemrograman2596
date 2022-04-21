439f77308acb4551784c8f1f9cfa6a6069e003fa
raihan <budhi.raihan@students.amikom.ac.id>
2022-04-21 15:10:33 +0700

    ResponsiPemrograman2596

---
+++ ResponsiPemrograman2596/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2596
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gaji1, gaji2;
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+            Console.WriteLine("No Nik/nama        Gaji Bulanan");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1. " + karyawan1.Nik + " " + karyawan1.Nama + "\t" + karyawan1.gaji_bulanan);
+            Console.WriteLine("2. " + karyawan2.Nik + " " + karyawan2.Nama + "\t" + karyawan2.gaji_bulanan);
+            gaji1 = (10 * karyawan1.gaji_bulanan) / 100;
+            gaji2 = (10 * karyawan2.gaji_bulanan) / 100;
+            Console.WriteLine("\nasyik kenaikan gaji 10%!\n");
+            Console.WriteLine("No Nik/nama        Gaji Bulanan");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1. " + karyawan1.Nik + " " + karyawan1.Nama + "\t" + (gaji1 + karyawan1.gaji_bulanan));
+            Console.WriteLine("2. " + karyawan2.Nik + " " + karyawan2.Nama + "\t" + (gaji2 + karyawan2.gaji_bulanan));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ResponsiPemrograman2596/karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2596
+{
+    class Karyawan
+    {
+        public string Nik;
+        public string Nama;
+        public int gaji_bulanan;
+        public Karyawan(string nik, string nama, int gaji_bulanan)
+        {
+            this.Nik = nik;
+            this.Nama = nama;
+            if (gaji_bulanan < 0)
+            {
+                this.Nik = "0";
+            }
+            else
+            {
+                this.gaji_bulanan = gaji_bulanan;
+            }
+        }
+    }
+}
